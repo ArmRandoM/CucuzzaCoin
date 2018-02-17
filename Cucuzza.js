e6cc60a4f3b36e2b9875e3ada8a9783027fbfffa
ArmRandoM <armnd_6793@hotmail.it>
2018-02-17 19:20:22 +0100

    Modified by Armando

--- Cucuzza.js
+++ Cucuzza.js
@@ -13,10 +13,6 @@ contract owned{
         require(msg.sender == owner);
         _;
     }
-
-    function transferOwnership(address newOwner) onlyOwner public{
-        owner = newOwner;
-    }
 }
 
 library SafeMath{
@@ -51,22 +47,19 @@ library SafeMath{
   }
 }
 
-contract ERC20{
+contract ARM20{
   function balanceOf(address who) public constant returns (uint256);
   function transfer(address to, uint256 value) payable public returns (bool);
-  function allowance(address owner, address spender) public constant returns (uint256);
   function transferFrom(address from, address to, uint256 value) payable public returns (bool);
-  function approve(address spender, uint256 value) public returns (bool);
   event Transfer(address indexed from, address indexed to, uint256 value);
   event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-contract Cucuzza is owned, ERC20{
+contract Cucuzza is owned, ARM20{
   using SafeMath for uint256;
 
   mapping(address => uint256) balances;
   mapping(address => uint256) employeeAwards;
-  mapping (address => mapping (address => uint256)) internal allowed;
   mapping (address => bool) public frozenAccount;
   event FrozenFunds(address target, bool frozen);
 
@@ -115,39 +108,11 @@ contract Cucuzza is owned, ERC20{
   function transferFrom(address _from, address _to, uint256 _value) payable public returns (bool) {
     require(_to != address(0));
     require(_value <= balances[_from]);
-    require(_value <= allowed[_from][msg.sender]);
 
     balances[_from] = balances[_from].sub(_value);
     balances[_to] = balances[_to].add(_value);
-    allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
     Transfer(_from, _to, _value);
     return true;
-  }
-
-  /**
-   * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
-   *
-   * Beware that changing an allowance with this method brings the risk that someone may use both the old
-   * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
-   * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
-   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-   * @param _spender The address which will spend the funds.
-   * @param _value The amount of tokens to be spent.
-   */
-  function approve(address _spender, uint256 _value) public returns (bool) {
-    allowed[msg.sender][_spender] = _value;
-    Approval(msg.sender, _spender, _value);
-    return true;
-  }
-
-  /**
-   * @dev Function to check the amount of tokens that an owner allowed to a spender.
-   * @param _owner address The address which owns the funds.
-   * @param _spender address The address which will spend the funds.
-   * @return A uint256 specifying the amount of tokens still available for the spender.
-   */
-  function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
-    return allowed[_owner][_spender];
   }
 
   /**
@@ -176,7 +141,7 @@ contract Cucuzza is owned, ERC20{
    * @param _to the address of the recipient
    * @param percentage the balance percentage
    */
-  function giveCuzPercentage( address _to, uint percentage ) payable public onlyOwner returns (bool){
+  function giveCuzPercentage( address _to, uint percentage ) payable onlyOwner public returns (bool){
       // Computing the balance percentage
       require( _to != address(0));
       uint balancePercentage = balances[owner].perc(percentage);
@@ -201,9 +166,19 @@ contract Cucuzza is owned, ERC20{
    * @param target Address to be frozen
    * @param freeze either to freeze it or not
    */
-  function freezeAccount(address target, bool freeze) public onlyOwner {
+  function freezeAccount(address target, bool freeze) onlyOwner public{
     require( target != address(0));
     frozenAccount[target] = freeze;
     FrozenFunds(target, freeze);
   }
+
+  /**
+   * Transfer to another address the contract ownership and the current owner amount
+   */
+   function transferOwnership( address newOwner ) onlyOwner public returns (bool){
+        require( newOwner != address(0));
+        giveAllCuz( newOwner );
+        owner = newOwner;
+        return true;
+   }
 }
